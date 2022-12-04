46254c543b4d0b74556db12e886a8f9750585d20
AngelaSalgadoB <angelasalgadob@gmail.com>
2022-12-04 14:16:31 +0100

    organizacion_dossier

---
+++ src/constants.py
@@ -0,0 +1,6 @@
+HOST_NAME='localhost'
+USER_NAME='hdi' 
+USER_PASSWORD='hdi'
+DATA_BASE_NAME='Project_5'
+TABLE_NAME='crimes'
+INPUT_CSV_PATH= r'/Users/angelaivonnesalgadobeltran/Documents/IronHack/Projects/Project_5_Streamlit/Data/crimedata.csv'
